ffc826f421f9110e0ef19bfbc2289156f5f3a4d1
myrahkis <kruzinka@yandex.ru>
2025-02-01 16:30:27 +0300

    last section

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -1,4 +1,5 @@
 <script setup>
+import CommunityCard from './CommunityCard.vue'
 import InfoBlock from './InfoBlock.vue'
 </script>
 
@@ -88,6 +89,31 @@ import InfoBlock from './InfoBlock.vue'
       </div>
     </div>
   </section>
+  <section class="community-section">
+    <h2 class="heading-secondary u-text-center u-mb-s">Caring is the new marketing</h2>
+    <p class="com-description u-text-center u-mb-m">
+      The Nexcent blog is the best place to read about the latest membership insights, trends and
+      more. See who's joining the community, read about how our community are increasing their
+      membership income and lot's more.
+    </p>
+    <div class="community-cards">
+      <CommunityCard
+        imgPath="/img/comImg1.png"
+        heading="Creating Streamlined Safeguarding Processes with OneRen"
+        btnText="Read more"
+      />
+      <CommunityCard
+        imgPath="/img/comImg2.png"
+        heading="What are your safeguarding responsibilities and how can you manage them?"
+        btnText="Read more"
+      />
+      <CommunityCard
+        imgPath="/img/comImg3.png"
+        heading="Revamping the Membership Model with Triathlon Australia"
+        btnText="Read more"
+      />
+    </div>
+  </section>
 </template>
 
 <style scoped>
@@ -169,5 +195,26 @@ import InfoBlock from './InfoBlock.vue'
 .customers-btn::after {
   content: '→';
   margin-left: 8px;
+}
+
+.community-section {
+  display: flex;
+  flex-direction: column;
+  margin-top: 48px;
+  margin-bottom: 80px;
+  height: 506px;
+}
+.community-cards {
+  display: flex;
+  justify-content: space-between;
+  padding: 0 144px;
+}
+.com-description {
+  width: 628px;
+  align-self: center;
+  font-weight: 400;
+  line-height: 24px;
+  font-size: 16px;
+  color: var(--Grey);
 }
 </style>
---
+++ src/components/CommunityCard.vue
@@ -0,0 +1,58 @@
+<script setup>
+const { imgPath, heading, btnText } = defineProps({
+  imgPath: String,
+  heading: String,
+  btnText: String,
+})
+</script>
+
+<template>
+  <div class="community-card">
+    <img :src="imgPath" alt="" />
+    <div class="card-content u-shadow-m">
+      <h4 class="heading-quaternary u-text-center">{{ heading }}</h4>
+      <button class="readmore-btn">{{ btnText }}</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.community-card {
+  position: relative;
+  width: 368px;
+  height: 286px;
+}
+.card-content {
+  position: absolute;
+  top: 190px;
+  left: 25.5px;
+  z-index: 1;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 16px;
+  background-color: var(--Silver);
+  padding: 16px;
+  width: 317px;
+  height: 176px;
+  border-radius: 8px;
+}
+.heading-quaternary {
+  color: var(--Grey);
+}
+.readmore-btn {
+    cursor: pointer;
+  color: var(--Primary);
+  background-color: transparent;
+  border: none;
+  line-height: 28px;
+  font-size: 20px;
+  font-weight: 600;
+}
+.readmore-btn::after {
+  content: '→';
+  margin-left: 8px;
+}
+</style>
